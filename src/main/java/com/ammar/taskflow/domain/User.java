8974e65c28cda88d2723a0a14f2d5f8ea666df82
Ammar SA <ammar.ahmad.sameed@gmail.com>
2026-09-11 19:40:35 +0300

    finishing the user table

--- src/main/java/com/ammar/taskflow/domain/User.java
+++ src/main/java/com/ammar/taskflow/domain/User.java
@@ -9,13 +9,38 @@ import java.util.UUID;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    UUID id;
+    private UUID id;
 
     @Column(unique = false , nullable = false)
-    String name;
+    private String name;
 
     @Column(unique = true , nullable = false)
-    String email;
+    private String email;
 
+    public User(){}
 
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
